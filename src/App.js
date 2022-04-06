aa44d45cb63a26631d82f6fdcfbeef9a01ce162a
mkrtchyan_van mail.ru <mkrtchyan_van@mail.ru>
2022-04-06 20:59:02 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("");
+    const [masOfcomp, masComp] = useState([])
+    const changeHandler = (name) =>{
+        setName(name)
+        const urlcomp= ("https://autocomplete.clearbit.com/v1/companies/suggest?query=")
+        const loadcompanies = async ()=> {
+            const response = await axios.get(urlcomp + name);
+            masComp(response.data)
+        }
+        loadcompanies();
+        console.log(masOfcomp)
+    }
+    const clickhandler = (e)=>{
+        setName(e.target.textContent)
+    }
+
+    return (
+        <div className="company_search">
+
+            <form action="">
+                <h1>Компания</h1>
+                <input
+                    type="text"
+                    name="name"
+                    className="search_space"
+                    autoComplete="off"
+                    onChange={e => changeHandler(e.target.value)}
+                    value={name} />
+
+                <ul className="autocomplete_list">
+
+                    {masOfcomp.map((company)=>{
+                        if (name) {
+                            return <li className="list_item" onClick={clickhandler}>
+                                <img src={company.logo} alt="logo"/>
+                                <div className="namecomp">
+                                    <p>{company.name}</p>
+                                    <a href={company.domain}>{company.domain}</a>
+                                </div>
+                            </li>
+                        }
+                    })
+                    }
+                </ul>
+            </form>
+
+        </div>
+
+    );
 }
 
 export default App;
